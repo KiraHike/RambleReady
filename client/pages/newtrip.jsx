b60b3cfea010659206de899a09bfd040858d6727
KiraHike <kirahikes@gmail.com>
2021-04-06 12:09:18 -0700

    added class to submit button #1

--- client/pages/newtrip.jsx
+++ client/pages/newtrip.jsx
@@ -11,7 +11,7 @@ export default class NewTrip extends React.Component {
           <input required type='date' name='date-end' />
           <input required type='text' name='country' />
           <input required type='number' name='budget' min='1' />
-          <button type='submit'>SAVE</button>
+          <button type='submit' className='button-save'>SAVE</button>
         </form>
       </div>
     );
