551dff6ab326aa986be4d6147ef51ce669f39f49
KiraHike <kirahikes@gmail.com>
2021-04-06 11:09:07 -0700

    added row and column elements

--- client/pages/trips.jsx
+++ client/pages/trips.jsx
@@ -6,9 +6,21 @@ export default class Trips extends React.Component {
   render() {
     return (
       <>
-      <h2>My Trips</h2>
-      <button>Add New Trip</button>
-      <TripCard />
+      <div className='row'>
+        <div className='column-full'>
+            <h2>My Trips</h2>
+        </div>
+      </div>
+      <div className='row'>
+        <div className='column-full'>
+            <button className='button-add-trip'>Add New Trip</button>
+        </div>
+      </div>
+      <div className='row'>
+        <div className='column-full'>
+            <TripCard />
+        </div>
+      </div>
       </>
     );
   }
