629fb3d1a178be5053ffeded06d18d56ca0337ac
KiraHike <kirahikes@gmail.com>
2021-04-06 15:38:42 -0700

    added container div to trips page #1

--- client/pages/trips.jsx
+++ client/pages/trips.jsx
@@ -5,23 +5,23 @@ export default class Trips extends React.Component {
 
   render() {
     return (
-      <>
-      <div className='row'>
-        <div className='column-full'>
+      <div className='container'>
+        <div className='row'>
+          <div className='column-full'>
             <h2>My Trips</h2>
+          </div>
         </div>
-      </div>
-      <div className='row'>
-        <div className='column-full'>
+        <div className='row'>
+          <div className='column-full'>
             <button className='button-add-trip'>Add New Trip</button>
+          </div>
         </div>
-      </div>
-      <div className='row'>
-        <div className='column-full'>
+        <div className='row'>
+          <div className='column-full'>
             <TripCard />
+          </div>
         </div>
       </div>
-      </>
     );
   }
 }
