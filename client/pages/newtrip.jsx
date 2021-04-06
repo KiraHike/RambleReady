195e8ecc1ae1cec9d80909d9f8398c780f5c11c8
KiraHike <kirahikes@gmail.com>
2021-04-06 11:53:54 -0700

    added new trip form page #1

---
+++ client/pages/newtrip.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default class NewTrip extends React.Component {
+  render() {
+    return (
+      <div className='form-container'>
+        <button className='button-close'>X</button>
+        <h3>New Trip</h3>
+        <form className='form-new-trip'>
+          <input required type='date' name='date-start' />
+          <input required type='date' name='date-end' />
+          <input required type='text' name='country' />
+          <input required type='number' name='budget' min='1' />
+          <button type='submit'>SAVE</button>
+        </form>
+      </div>
+    );
+  }
+}
