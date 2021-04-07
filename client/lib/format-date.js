71c5d7559a50116a6cc4affa1282efedded66274
KiraHike <kirahikes@gmail.com>
2021-04-07 16:44:36 -0700

    adjusted format date function #14

--- client/lib/format-date.js
+++ client/lib/format-date.js
@@ -2,7 +2,7 @@ const parseISO = require('date-fns/parseISO');
 
 function formatDate(iso) {
   const parsedDate = parseISO(iso);
-  const month = parsedDate.getMonth();
+  const month = parsedDate.getMonth() + 1;
   const day = parsedDate.getDate();
   const year = parsedDate.getFullYear();
   const formattedDate = `${month}/${day}/${year}`;
