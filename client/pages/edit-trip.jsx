3d8b1aa6360c02322444db22408b34bdddb8b4b2
KiraHike <kirahikes@gmail.com>
2021-04-08 17:37:18 -0700

    added confirm delete elements #3

--- client/pages/edit-trip.jsx
+++ client/pages/edit-trip.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import AppContext from '../lib/app-context';
-import formatDateEdit from '../lib/format-date-edit';
+import dateYearMonthDay from '../lib/format-date-ymd';
+import dateMonthDayYear from '../lib/format-date-mdy';
 
 export default class EditTrip extends React.Component {
   constructor(props) {
@@ -71,18 +72,25 @@ export default class EditTrip extends React.Component {
 
   render() {
     if (this.state.isDeleting) {
+      const startDateMonthDayYear = dateMonthDayYear(this.state.startDate);
+      const endDateMonthDayYear = dateMonthDayYear(this.state.endDate);
       return (
         <div className='container confirm-delete'>
           <button className='button-close' onClick={this.handleClose}>x</button>
           <div>
-            Trip Details
+            <h3>Delete Trip?</h3>
+            <div className='trip-delete-container'>
+              <h4 className='trip-delete-country'>{this.state.country}</h4>
+              <p className='trip-delete-date'>{startDateMonthDayYear} - {endDateMonthDayYear}</p>
+              <p className='trip-delete-budget'>Budget: ${this.state.budget}</p>
+            </div>
         </div>
-          <button className='fas fa-check-square' onClick={this.handleConfirmDelete} />
+          <button className='fas fa-check-square button-confirm' onClick={this.handleConfirmDelete} />
         </div >
       );
     } else {
-      const formattedStartDate = formatDateEdit(this.state.startDate);
-      const formattedEndDate = formatDateEdit(this.state.endDate);
+      const startDateYearMonthDay = dateYearMonthDay(this.state.startDate);
+      const endDateYearMonthDay = dateYearMonthDay(this.state.endDate);
       return (
         <div className='container new-trip'>
           <div className='form-container'>
@@ -92,9 +100,9 @@ export default class EditTrip extends React.Component {
             <h3>Edit Trip</h3>
             <form className='form-new-trip' onSubmit={this.handleSubmit}>
               <label htmlFor='startDate' className='text-dark-blue'>Start Date</label>
-              <input required type='date' name='startDate' className='border-dark-blue' value={formattedStartDate} onChange={this.handleChange} />
+              <input required type='date' name='startDate' className='border-dark-blue' value={startDateYearMonthDay} onChange={this.handleChange} />
               <label htmlFor='endDate' className='text-dark-blue'>End Date</label>
-              <input required type='date' name='endDate' className='border-dark-blue' value={formattedEndDate} onChange={this.handleChange} />
+              <input required type='date' name='endDate' className='border-dark-blue' value={endDateYearMonthDay} onChange={this.handleChange} />
               <label htmlFor='country' className='text-blue'>Country</label>
               <input required type='text' name='country' className='border-blue' value={this.state.country} onChange={this.handleChange} />
               <label htmlFor='budget' className='text-light-blue'>Budget</label>
