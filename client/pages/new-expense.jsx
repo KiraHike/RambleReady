6c57db8ed32527389a9ebfdcee45668d9cdc1bfc
KiraHike <kirahikes@gmail.com>
2021-04-08 18:40:34 -0700

    added new expense page #4

---
+++ client/pages/new-expense.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import AppContext from '../lib/app-context';
+
+export default class NewExpense extends React.Component {
+
+  render() {
+
+    return (
+
+    );
+  }
+}
+
+NewExpense.contextType = AppContext;
