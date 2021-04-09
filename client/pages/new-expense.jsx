e93227f296effc921203cc31760cdbafa1d19924
KiraHike <kirahikes@gmail.com>
2021-04-08 18:45:27 -0700

    added date currency and exchange to state #4

--- client/pages/new-expense.jsx
+++ client/pages/new-expense.jsx
@@ -2,11 +2,21 @@ import React from 'react';
 import AppContext from '../lib/app-context';
 
 export default class NewExpense extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: '',
+      currency: '',
+      exchange: ''
+    };
+
+  }
 
   render() {
 
     return (
-
+      <>
+      </>
     );
   }
 }
