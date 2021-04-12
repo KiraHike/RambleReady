3732b42e6f6ed43540ac9589d260f744464ec245
KiraHike <kirahikes@gmail.com>
2021-04-12 13:15:48 -0700

    added countries drop down select to new trip form #5

--- client/pages/newtrip.jsx
+++ client/pages/newtrip.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import AppContext from '../lib/app-context';
+import Countries from '../components/countries';
 
 export default class NewTrip extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      countries: [],
+      currency: '',
       startDate: '',
       endDate: '',
       country: '',
@@ -12,6 +15,14 @@ export default class NewTrip extends React.Component {
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount(event) {
+    fetch('/api/countries')
+      .then(res => res.json())
+      .then(countries => {
+        this.setState({ countries });
+      });
   }
 
   handleChange(event) {
@@ -35,7 +46,6 @@ export default class NewTrip extends React.Component {
   }
 
   render() {
-
     return (
       <div className='container new-trip'>
         <div className='form-container'>
@@ -44,14 +54,14 @@ export default class NewTrip extends React.Component {
           </a>
           <h3>New Trip</h3>
           <form className='form-new-trip' onSubmit={this.handleSubmit}>
-            <label htmlFor='startDate' className='text-dark-blue'>Start Date</label>
-            <input required type='date' name='startDate' className='border-dark-blue' onChange={this.handleChange} />
-            <label htmlFor='endDate' className='text-dark-blue'>End Date</label>
-            <input required type='date' name='endDate' className='border-dark-blue' onChange={this.handleChange} />
-            <label htmlFor='country' className='text-blue'>Country</label>
-            <input required type='text' name='country' className='border-blue' onChange={this.handleChange} />
-            <label htmlFor='budget' className='text-light-blue'>Budget</label>
-            <input required type='number' name='budget' min='1' className='border-light-blue' onChange={this.handleChange} />
+            <label htmlFor='startDate' className='text-dark-blue new-trip-label'>Start Date</label>
+            <input required type='date' name='startDate' className='border-dark-blue new-trip-input' onChange={this.handleChange} />
+            <label htmlFor='endDate' className='text-dark-blue new-trip-label'>End Date</label>
+            <input required type='date' name='endDate' className='border-dark-blue new-trip-input' onChange={this.handleChange} />
+            <label htmlFor='country' className='text-blue new-trip-label'>Country</label>
+            <Countries value={this.value} onChange={this.handleChange} countriesArray = {this.state.countries} />
+            <label htmlFor='budget' className='text-light-blue new-trip-label'>Budget</label>
+            <input required type='number' name='budget' min='1' className='border-light-blue new-trip-input' onChange={this.handleChange} />
             <button type='submit' className='button-save'>SAVE</button>
           </form>
         </div>
