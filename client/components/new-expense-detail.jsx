c198df72ea7f1510412e4faa7bf66f5b90bde682
KiraHike <kirahikes@gmail.com>
2021-04-13 15:10:24 -0700

    added toggle handle #5

--- client/components/new-expense-detail.jsx
+++ client/components/new-expense-detail.jsx
@@ -4,7 +4,23 @@ import ToggleSwitch from './toggle-switch';
 export default class NewExpenseDetail extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      tripId: null,
+      date: null,
+      category: null,
+      subcategory: null,
+      notes: null,
+      amount: null,
+      toggleOn: false
+    };
     this.makeOptions = this.makeOptions.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleToggle = this.handleToggle.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState({ tripId: this.props.tripId });
   }
 
   makeOptions(array) {
@@ -13,33 +29,60 @@ export default class NewExpenseDetail extends React.Component {
     } else {
       const optionsList = array.map(option => {
         return (
-          <option key={option}>{option}</option>
+          <option value={option} key={option}>{option}</option>
         );
       });
       return (
-        <select className='subcategory-select'>
+        <select value={this.value} name='subcategory' className='subcategory-select'>
+          <option>Select</option>
           {optionsList}
         </select>
       );
     }
   }
 
+  handleChange(event) {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  }
+
+  handleToggle(event) {
+    (this.state.toggleOn)
+      ? this.setState({ toggleOn: false })
+      : this.setState({ toggleOn: true });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    fetch('/api/expenses', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(this.state)
+    })
+      .then(res => {
+        res.json();
+        this.setState({ view: null });
+      });
+  }
+
   render() {
     const subcategoryOptions = this.makeOptions(this.props.subcategoryArray);
     return (
-      <form className='form-new-expense'>
+      <form onChange={this.handleChange} className='form-new-expense'>
         <label htmlFor='amount' />
-        <input required type='number' name='amount' className='subcategory-amount' placeholder='Amount' />
+        <input required value={this.value} type='number' name='amount' className='subcategory-amount' placeholder='Amount' />
         {subcategoryOptions}
         <div className='toggle-container'>
           <p className='toggle-currency'>{this.props.currency}</p>
-          <ToggleSwitch />
+          <ToggleSwitch toggle={this.state.toggleOn} onClick={this.handleToggle} />
           <p className='toggle-currency'>USD</p>
         </div>
         <label htmlFor='notes' />
-        <input type='textarea' name='notes' className='expense-notes' placeholder='Notes (Optional)' />
-        <label htmlFor='expenseDate' />
-        <input required type='date' name='expenseDate' className='expense-date' />
+        <input value={this.value} type='textarea' name='notes' className='expense-notes' placeholder='Notes (Optional)' />
+        <label htmlFor='date' />
+        <input value={this.value} required type='date' name='date' className='expense-date' />
         <button type='submit' className='button-add-expense'>SAVE</button>
       </form>
     );
