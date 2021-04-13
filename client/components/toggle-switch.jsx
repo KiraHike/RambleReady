7c5930cece205d6df18b89545e0932c0d6b8d2cb
KiraHike <kirahikes@gmail.com>
2021-04-13 15:07:54 -0700

    removed state and handle in component to pass by props #5

--- client/components/toggle-switch.jsx
+++ client/components/toggle-switch.jsx
@@ -1,25 +1,13 @@
 import React from 'react';
 
 export default class ToggleSwitch extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { on: false };
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick(event) {
-    (this.state.on)
-      ? this.setState({ on: false })
-      : this.setState({ on: true });
-  }
-
   render() {
-    if (this.state.on) {
+    if (this.props.toggle) {
       return (
         <div className="switch off">
           <input className="toggle-switch" type="checkbox" />
           <span className="toggle off" />
-          <label htmlFor="toggle-switch" className="switch-label" onClick={this.handleClick}></label>
+          <label htmlFor="toggle-switch" className="switch-label" onClick={this.props.onClick}></label>
         </div>
       );
     }
@@ -27,7 +15,7 @@ export default class ToggleSwitch extends React.Component {
       <div className="switch on">
         <input className="toggle-switch" type="checkbox" />
         <span className="toggle on" />
-        <label htmlFor="toggle-switch" className="switch-label" onClick={this.handleClick}></label>
+        <label htmlFor="toggle-switch" className="switch-label" onClick={this.props.onClick}></label>
       </div>
     );
   }
