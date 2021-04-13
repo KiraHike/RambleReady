81b5d39da1c7e992829b0f0a552cc6cbc5ac3e5a
KiraHike <kirahikes@gmail.com>
2021-04-13 10:44:11 -0700

    added render with disabled buttons if no trip is chosen #5

--- client/pages/new-expense.jsx
+++ client/pages/new-expense.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import AppContext from '../lib/app-context';
 import NewExpenseDetail from '../components/new-expense-detail';
 import calculate from '../lib/calculateUSD';
+import TripSelect from '../components/trip-select';
 
 const expenseCategories = {
   foodAndDrink: {
@@ -46,19 +47,32 @@ export default class NewExpense extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      currency: 'EUR',
-      convert: null,
-      usd: null,
-      amount: null,
+      trips: [],
+      tripId: null,
+      date: null,
+      category: null,
       subcategory: null,
       notes: null,
-      date: null,
+      currency: null,
+      exchangeRate: null,
+      amount: null,
+      usd: null,
+
       view: null
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleClick = this.handleClick.bind(this);
     this.handleClose = this.handleClose.bind(this);
     this.convertCurrency = this.convertCurrency.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    fetch('/api/trips')
+      .then(res => res.json())
+      .then(trips => {
+        this.setState({ trips });
+      });
   }
 
   handleChange(event) {
@@ -68,7 +82,7 @@ export default class NewExpense extends React.Component {
 
   handleClick(event) {
     if (event.target.matches('button')) {
-      window.location.hash = `#newexpense?currency=${this.state.currency}`;
+      window.location.hash = `#newexpense?tripId=${this.state.tripId}`;
       this.setState({ view: event.target.id });
     }
   }
@@ -84,12 +98,27 @@ export default class NewExpense extends React.Component {
         return result.json();
       })
       .then(data => {
-        this.setState({ usd: calculate(this.state.convert, data.conversion_rate) });
+        this.setState({ exchangeRate });
       })
       .catch(err => {
         console.error(err);
       });
 
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    fetch('/api/expenses', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(this.state)
+    })
+      .then(res => {
+        res.json();
+        this.setState({ view: null });
+      });
   }
 
   render() {
@@ -100,11 +129,39 @@ export default class NewExpense extends React.Component {
           <div className='expense-form-container'>
             <button className='button-close right' onClick={this.handleClose}>x</button>
             <h3>{expenseCategories[type].category}</h3>
-            <NewExpenseDetail category={expenseCategories[type].category} subcategoryArray={expenseCategories[type].subcategories} />
+            <NewExpenseDetail onChange={this.handleChange} value={this.value} category={expenseCategories[type].category} subcategoryArray={expenseCategories[type].subcategories} onSubmit={this.handleSubmit}/>
           </div>
         </div>
       );
     }
+
+    if (!this.state.tripId) {
+      return (
+      <div className='container'>
+        <form>
+          <TripSelect tripArray={this.state.trips} onChange={this.handleChange} value={this.value} />
+        </form>
+        <div className='icon-container'>
+          <button id='foodAndDrink' className='fas fa-utensils icon food' />
+          <button id='localTransportation' className='fas fa-bus icon local-transportation' />
+          <button id='shopping' className='fas fa-gift icon shopping' />
+          <button id='entertainment' className='fas fa-theater-masks icon entertainment' />
+          <button id='lodging' className='fas fa-bed icon lodging' />
+          <button id='internationalTransportation' className='fas fa-plane-departure icon international-transportation' />
+          <button id='administrative' className='fas fa-globe icon administrative' />
+          <button id='medical' className='fas fa-heartbeat icon medical' />
+          <button id='miscellaneous' className='fas fa-asterisk icon misc' />
+        </div>
+        <div className='converter-container'>
+          <form className='converter-form'>
+            <input disabled type='number' name='convert' className='converter-input' />
+            <button disabled type='submit' className='fas fa-calculator icon calculator' />
+          </form>
+        </div>
+      </div>
+      );
+    }
+
     let converterResult;
     (this.state.usd)
       ? converterResult = `$${this.state.usd}`
@@ -113,9 +170,7 @@ export default class NewExpense extends React.Component {
     return (
       <div className='container'>
         <form>
-          <select className='country-select'>
-            <option>Country</option>
-          </select>
+          <TripSelect tripArray={this.state.trips} onChange={this.handleChange} value={this.value} />
         </form>
         <div className='icon-container' onClick={this.handleClick}>
           <button id='foodAndDrink' className='fas fa-utensils icon food' />
