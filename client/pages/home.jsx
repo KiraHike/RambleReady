e42675a2bdaa835f47b79531006b2111ced07dbc
KiraHike <kirahikes@gmail.com>
2021-04-14 16:53:48 -0700

    adjusted fetch request to work with sum aggregate #6

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import AppContext from '../lib/app-context';
 import TripSelect from '../components/trip-select';
 
 export default class Home extends React.Component {
@@ -8,8 +7,8 @@ export default class Home extends React.Component {
     this.state = {
       trips: [],
       trip: null,
-      tripSum: null,
-      tripBalance: null
+      tripSum: 0,
+      tripBalance: 0
     };
     this.handleSelect = this.handleSelect.bind(this);
   }
@@ -25,21 +24,14 @@ export default class Home extends React.Component {
   handleSelect(event) {
     fetch(`/api/trips/${event.target.value}`)
       .then(res => res.json())
-      .then(data => {
-        const trip = data;
-        return fetch(`/api/expenses/${trip.tripId}`)
-          .then(res => res.json())
-          .then(expenses => {
-            if (expenses.length < 1) {
-              this.setState({ trip, tripSum: null, tripBalance: 0 });
-            } else {
-              const tripSum = expenses.reduce((accumulator, currentValue) => {
-                return accumulator + Number(currentValue.amount);
-              }, Number(expenses[0].amount)).toFixed(2);
-              const tripBalance = (Number(trip.budget) - tripSum).toFixed(2);
-              this.setState({ trip, tripSum, tripBalance });
-            }
-          });
+      .then(trip => {
+        if (!trip.tripSum) {
+          this.setState({ trip, tripSum: 0, tripBalance: 0 });
+        } else {
+          const tripSum = Number(trip.tripSum);
+          const tripBalance = (Number(trip.budget) - tripSum).toFixed(2);
+          this.setState({ trip, tripSum, tripBalance });
+        }
       })
       .catch(err => {
         console.error(err);
@@ -71,11 +63,9 @@ export default class Home extends React.Component {
         </form>
         <div className='trip-balance'>
           {`Balance:  $${this.state.tripBalance}`}
-          <progress className='progress-bar' max={Number(this.state.trip.budget)} value={this.state.tripSum} />
+          <progress className='progress-bar' max={Number(this.state.trip.budget)} value={this.state.trip.tripSum} />
         </div>
       </div>
     );
   }
 }
-
-Home.contextType = AppContext;
