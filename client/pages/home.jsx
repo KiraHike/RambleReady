19d46ac53d5098ef84bee8d071fcfbb120558158
KiraHike <kirahikes@gmail.com>
2021-04-14 10:48:46 -0700

    added home page #6

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
-export default function Home(props) {
-  return (
-    <>
-    </>
-  );
+export default class Home extends React.Component {
+
+  render() {
+    return (
+      <div className='container'>
+      </div>
+    );
+  }
 }
