2b50590a7d6188bc2344f11466f39c5c050d3071
KiraHike <kirahikes@gmail.com>
2021-04-14 10:54:46 -0700

    added  trip select drop down #6

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,11 +1,46 @@
 import React from 'react';
+import AppContext from '../lib/app-context';
+import TripSelect from '../components/trip-select';
 
 export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      trips: []
+    };
+    this.handleSelect = this.handleSelect.bind(this);
+  }
+
+  componentDidMount() {
+    fetch('/api/trips')
+      .then(res => res.json())
+      .then(trips => {
+        this.setState({ trips });
+      });
+  }
+
+  handleSelect(event) {
+    const id = event.target.value;
+    fetch(`/api/trips/${id}`)
+      .then(res => res.json())
+      .then(trip => {
+        this.setState({ tripId: trip.tripId, currency: trip.currency });
+      });
+  }
 
   render() {
     return (
       <div className='container'>
+        <form>
+          <TripSelect
+            tripArray={this.state.trips}
+            onChange={this.handleSelect}
+            value={this.value}
+          />
+        </form>
       </div>
     );
   }
 }
+
+Home.contextType = AppContext;
