e833446ac95ce4f0042e64214cd4029341ea5e58
KiraHike <kirahikes@gmail.com>
2021-04-15 13:25:33 -0700

    modified trip length function to calculate remaining trip days instead #8

---
+++ client/lib/remaining-days.js
@@ -0,0 +1,14 @@
+const differenceInDays = require('date-fns/differenceInCalendarDays');
+
+export default function remainingDays(startISO, endISO) {
+  const startDate = new Date(startISO);
+  const endDate = new Date(endISO);
+  let days;
+  if (new Date() >= startDate) {
+    days = differenceInDays(new Date(), endDate);
+  } else {
+    days = differenceInDays(startDate, endDate);
+
+  }
+  return Math.abs(days) + 1;
+}
