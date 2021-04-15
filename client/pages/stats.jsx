4d7e00a67e4cebdff2c80bf492d64aa97914fcea
KiraHike <kirahikes@gmail.com>
2021-04-15 15:26:14 -0700

    added trip select drop down to stats page #9

--- client/pages/stats.jsx
+++ client/pages/stats.jsx
@@ -1,10 +1,43 @@
 import React from 'react';
+import TripSelect from '../components/trip-select';
 
 export default class Stats extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      trips: [],
+      tripId: null
+    };
+    this.handleSelect = this.handleSelect.bind(this);
+  }
+
+  componentDidMount() {
+    fetch('/api/trips')
+      .then(res => res.json())
+      .then(trips => {
+        this.setState({ trips });
+      });
+  }
+
+  handleSelect(event) {
+    const id = event.target.value;
+    fetch(`/api/trips/${id}`)
+      .then(res => res.json())
+      .then(trip => {
+        this.setState({ tripId: trip.tripId });
+      });
+  }
 
   render() {
     return (
       <div className='container'>
+        <form>
+          <TripSelect
+            tripArray={this.state.trips}
+            onChange={this.handleSelect}
+            value={this.value}
+          />
+        </form>
       </div>
     );
   }
