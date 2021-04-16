3523917689696d9d253ebdf5250a00b22e8f27b0
KiraHike <kirahikes@gmail.com>
2021-04-16 16:46:16 -0700

    added function to render expense line items on click #9

--- client/pages/stats.jsx
+++ client/pages/stats.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import TripSelect from '../components/trip-select';
+import formatMonthDayYear from '../lib/format-date-mdy';
 
 export default class Stats extends React.Component {
   constructor(props) {
@@ -7,9 +8,11 @@ export default class Stats extends React.Component {
     this.state = {
       trips: [],
       tripId: null,
-      expenses: []
+      expenses: [],
+      isExpanded: false
     };
     this.handleSelect = this.handleSelect.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
   componentDidMount() {
@@ -24,11 +27,129 @@ export default class Stats extends React.Component {
     fetch(`/api/expenses/${event.target.value}`)
       .then(res => res.json())
       .then(expenses => {
-        this.setState({ expenses });
+        this.setState({ expenses, tripId: event.target.value });
       });
   }
 
+  handleClick(event) {
+    this.setState({ isExpanded: !this.state.isExpanded });
+  }
+
+  makeLineItems(array) {
+    const categoryHeaders = [
+      {
+        category: 'foodAndDrink',
+        header: 'Food & Drink'
+      },
+      {
+        category: 'localTransportation',
+        header: 'Local Transport'
+      },
+      {
+        category: 'shopping',
+        header: 'Shopping'
+      },
+      {
+        category: 'entertainment',
+        header: 'Entertainment'
+      },
+      {
+        category: 'lodging',
+        header: 'Lodging'
+      },
+      {
+        category: 'internationalTransportation',
+        header: 'International Transport'
+      },
+      {
+        category: 'administrative',
+        header: 'Administrative'
+      },
+      {
+        category: 'medical',
+        header: 'Medical'
+      },
+      {
+        category: 'miscellaneous',
+        header: 'Miscellaneous'
+      }
+    ];
+
+    const dates = array.map(date => {
+      const formattedDate = formatMonthDayYear(date.date);
+      const categories = date.expenses.map(category => {
+        let header;
+        let items;
+        for (let i = 0; i < categoryHeaders.length; i++) {
+          if (category.category === categoryHeaders[i].category) {
+            header = categoryHeaders[i].header;
+            items = category.expenses.map(item => {
+              return (
+                  <li key={item.expenseId}>
+                    <div>
+                      {item.subcategory}
+                      {'$' + (item.amount).toFixed(2)}
+                    </div>
+                    <div>
+                      {item.notes}
+                    </div>
+                  </li>
+              );
+            });
+          }
+        }
+        return (
+            <ul key={date.date + category.category}>
+              <h4>{header}</h4>
+              {items}
+            </ul>
+        );
+      });
+      return (
+      <ol key={date.date}>
+        {formattedDate}
+        {categories}
+        </ol>
+      );
+    });
+    return (
+      <>
+      {dates}
+      </>
+    );
+  }
+
   render() {
+
+    if (!this.state.tripId) {
+      return (
+        <div className='container'>
+          <form>
+            <TripSelect
+              tripArray={this.state.trips}
+              onChange={this.handleSelect}
+              value={this.value}
+            />
+          </form>
+        </div>
+      );
+    }
+
+    if (!this.state.isExpanded) {
+      return (
+        <div className='container'>
+          <form>
+            <TripSelect
+              tripArray={this.state.trips}
+              onChange={this.handleSelect}
+              value={this.value}
+            />
+          </form>
+            <button className='daily-expenses-button' onClick={this.handleClick}>Daily Expenses</button>
+        </div>
+      );
+    }
+    const lineItems = this.makeLineItems(this.state.expenses);
     return (
       <div className='container'>
         <form>
@@ -38,6 +159,10 @@ export default class Stats extends React.Component {
             value={this.value}
           />
         </form>
+          <button className='daily-expenses-button' onClick={this.handleClick}>Daily Expenses</button>
+        <div>
+        {lineItems}
+        </div>
       </div>
     );
   }
