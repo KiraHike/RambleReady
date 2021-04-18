e1fafa0dc911b416c6d7c69c1db03b51fbee5007
KiraHike <kirahikes@gmail.com>
2021-04-17 20:04:52 -0700

    added pie chart component to stats page #11

--- client/pages/stats.jsx
+++ client/pages/stats.jsx
@@ -1,6 +1,55 @@
 import React from 'react';
 import TripSelect from '../components/trip-select';
 import formatMonthDayYear from '../lib/format-date-mdy';
+import PieChart from '../components/pie-chart';
+
+const categoryHeaders = [
+  {
+    category: 'administrative',
+    header: 'Administrative',
+    total: 0
+  },
+  {
+    category: 'medical',
+    header: 'Medical',
+    total: 0
+  },
+  {
+    category: 'foodAndDrink',
+    header: 'Food & Drink',
+    total: 0
+  },
+  {
+    category: 'localTransportation',
+    header: 'Local Transport',
+    total: 0
+  },
+  {
+    category: 'shopping',
+    header: 'Shopping',
+    total: 0
+  },
+  {
+    category: 'entertainment',
+    header: 'Entertainment',
+    total: 0
+  },
+  {
+    category: 'lodging',
+    header: 'Lodging',
+    total: 0
+  },
+  {
+    category: 'internationalTransportation',
+    header: 'International Transport',
+    total: 0
+  },
+  {
+    category: 'miscellaneous',
+    header: 'Miscellaneous',
+    total: 0
+  }
+];
 
 export default class Stats extends React.Component {
   constructor(props) {
@@ -9,7 +58,9 @@ export default class Stats extends React.Component {
       trips: [],
       tripId: null,
       expenses: [],
-      isExpanded: false
+      isExpanded: false,
+      pieChartData: [],
+      lineItems: null
     };
     this.handleSelect = this.handleSelect.bind(this);
     this.handleClick = this.handleClick.bind(this);
@@ -23,58 +74,7 @@ export default class Stats extends React.Component {
       });
   }
 
-  handleSelect(event) {
-    fetch(`/api/expenses/${event.target.value}`)
-      .then(res => res.json())
-      .then(expenses => {
-        this.setState({ expenses, tripId: event.target.value });
-      });
-  }
-
-  handleClick(event) {
-    this.setState({ isExpanded: !this.state.isExpanded });
-  }
-
   makeLineItems(array) {
-    const categoryHeaders = [
-      {
-        category: 'foodAndDrink',
-        header: 'Food & Drink'
-      },
-      {
-        category: 'localTransportation',
-        header: 'Local Transport'
-      },
-      {
-        category: 'shopping',
-        header: 'Shopping'
-      },
-      {
-        category: 'entertainment',
-        header: 'Entertainment'
-      },
-      {
-        category: 'lodging',
-        header: 'Lodging'
-      },
-      {
-        category: 'internationalTransportation',
-        header: 'International Transport'
-      },
-      {
-        category: 'administrative',
-        header: 'Administrative'
-      },
-      {
-        category: 'medical',
-        header: 'Medical'
-      },
-      {
-        category: 'miscellaneous',
-        header: 'Miscellaneous'
-      }
-    ];
-
     const dates = array.map(date => {
       const formattedDate = formatMonthDayYear(date.date);
       const categories = date.expenses.map(category => {
@@ -84,6 +84,7 @@ export default class Stats extends React.Component {
           if (category.category === categoryHeaders[i].category) {
             header = categoryHeaders[i].header;
             items = category.expenses.map(item => {
+              categoryHeaders[i].total += item.amount;
               if (item.notes) {
                 return (
                   <li className='line-item-list-item-li' key={item.expenseId}>
@@ -125,6 +126,7 @@ export default class Stats extends React.Component {
         </ol>
       );
     });
+
     return (
       <>
         {dates}
@@ -132,7 +134,32 @@ export default class Stats extends React.Component {
     );
   }
 
+  makePieChartData(pieChartDataArray) {
+    const data = [];
+    for (let i = 0; i < pieChartDataArray.length; i++) {
+      data.push(pieChartDataArray[i].total.toFixed(2));
+    }
+    return data;
+  }
+
+  handleSelect(event) {
+    fetch(`/api/expenses/${event.target.value}`)
+      .then(res => res.json())
+      .then(expenses => {
+        const lineItems = this.makeLineItems(expenses);
+        const pieChartData = this.makePieChartData(categoryHeaders);
+        this.setState({ expenses, tripId: event.target.value, lineItems, pieChartData });
+      });
+  }
+
+  handleClick(event) {
+    this.setState({ isExpanded: !this.state.isExpanded });
+  }
+
   render() {
+    for (let i = 0; i < categoryHeaders.length; i++) {
+      categoryHeaders[i].total = 0;
+    }
 
     if (!this.state.tripId) {
       return (
@@ -157,10 +184,41 @@ export default class Stats extends React.Component {
             />
           </form>
           <button className='daily-expenses-button' onClick={this.handleClick}>Daily Expenses</button>
+          <div>
+            <h3 className='trip-expenses-header'>Trip Expenses:</h3>
+            <PieChart data={{
+              labels: [
+                `Admin. $${this.state.pieChartData[0]}`,
+                `Food $${this.state.pieChartData[1]}`,
+                `Med. $${this.state.pieChartData[2]}`,
+                `Trans. $${this.state.pieChartData[3]}`,
+                `Shop. $${this.state.pieChartData[4]}`,
+                `Ent. $${this.state.pieChartData[5]}`,
+                `Lodging $${this.state.pieChartData[6]}`,
+                `Int. Trans. $${this.state.pieChartData[7]}`,
+                `Misc. $${this.state.pieChartData[8]}`
+              ],
+              datasets: [{
+                label: 'Trip Expenses',
+                data: this.state.pieChartData,
+                backgroundColor: [
+                  'rgb(10, 103, 241)',
+                  'rgb(65, 195, 44)',
+                  'rgb(209, 40, 142)',
+                  'rgb(18, 61, 146)',
+                  'rgb(242, 34, 34)',
+                  'rgb(138, 12, 198)',
+                  'rgb(248, 126, 14)',
+                  'rgb(10, 172, 241)',
+                  'rgb(244, 221, 11)'
+                ]
+              }]
+            }} />
+          </div>
         </div>
       );
     }
-    const lineItems = this.makeLineItems(this.state.expenses);
+
     return (
       <div className='container'>
         <form>
@@ -171,7 +229,7 @@ export default class Stats extends React.Component {
         </form>
         <button className='daily-expenses-button' onClick={this.handleClick}>Daily Expenses</button>
         <div className='line-item-list-container'>
-          {lineItems}
+          {this.state.lineItems}
         </div>
       </div>
     );
